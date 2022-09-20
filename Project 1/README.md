770bebdb747c8a4a39a22c9137c116786f55835a
ibrahim <ibrahimshazad@Ibrahims-MacBook-Pro.local>
2022-09-20 03:46:22 -0500

    First commit

---
+++ Project 1/README.md
@@ -0,0 +1,28 @@
+## CSE 3330 Database 
+This is CSE3330 UTA Database project 
+
+### Getting Started
+Before you start make sure you have the Python 3.8 or higher installed in you operating system.<br/>
+
+### Initial Setup 
+* Clone the CSE3330 repo:
+    * To clone the repo run ```git clone https://github.com/ujjwalbgn/cse3330/ ```
+* Navigate into the repository  ```cd cse3330```
+
+### Create Database, Tables amd Insert Data
+* To create database (if not present), Create Table and Insert data into table run  ```createInsert.py``` which is located inside <b> databaseScripts</b> folder.<br/>
+<i>Rerunning ```createInsert.py``` will generate "UNIQUE constraint failed" error as the script will try to reinsert exisiting data into the database.</i>
+
+### 
+Running Database Query
+* To run the query run ```queries.py``` which is located inside <b> databaseScripts</b> folder.<br/>
+This will print out the results into the command prompt.
+
+* The arguments for the query functions can be changed depending on the needs: <br/>
+Example:
+    > #Enter a department name, and retrieve all the names and salaries of all employees who work in that department.
+
+   > ```getNameSalaries(conn, "Research")```</br>
+
+    To obtain info about another dept. we can replace ```"Research"``` with name of another dept. lets say ```"Sales"```.      
+    > ```getNameSalaries(conn, "Sales")```</br>
---
+++ Project 1/databaseScripts/queries.py
@@ -0,0 +1,104 @@
+import sqlite3
+from sqlite3 import Error
+
+def connection(db_file):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        print("Successfully Connected to SQLite")
+        return conn
+    except Error as e:
+        print("Error while connecting to sqlite")
+        print(e)
+   
+    return conn
+
+
+def getNameSalaries(conn, deptName):
+    print("Names and salaries of all employees who work in that department: " +deptName)
+    try:
+        cur = conn.cursor()
+        cur.execute("SELECT Fname, Minit,Lname, Salary FROM employee, department WHERE DName=? and DepartmentNumber = Dnumber ", (deptName,))
+
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+    except Error as e:
+        print(e)
+            
+
+
+def getProjNameHours(conn,lastName, firstName):
+    print("list of projects names/hours per week that the employee ("+firstName +" "+lastName +") works on")
+    try:
+        cur = conn.cursor()
+        cur.execute("SELECT Pname, Hours FROM project,employee, works_on WHERE Lname=? and Fname = ? and EmployeeSSN = Essn and Pno = Pnumber ", (lastName,firstName,))
+
+        rows = cur.fetchall()
+
+        for row in rows:
+            print(row)
+    except Error as e:
+        print(e)  
+
+def getTotalSalaries(conn, deptName):
+    print("Retrieve the total of all employee salaries who work in the department : " + deptName)
+    cur = conn.cursor()
+    cur.execute("SELECT SUM(Salary) FROM (employee JOIN department ON DepartmentNumber = Dnumber)WHERE DName =?", (deptName,))
+    try:
+        rows = cur.fetchall()
+
+        for row in rows:
+            print(row)
+    except Error as e:
+        print(e)  
+
+def getAllDeptEmp(conn):
+    print("For each department, retrieve the department name and the number (count) of employees who work in that department. Order the result by number of employees in descending order")
+    try:
+        cur = conn.cursor()
+        cur.execute("SELECT DName, COUNT(*) from department INNER JOIN employee ON DepartmentNumber = Dnumber GROUP BY DepartmentNumber, DName ORDER BY COUNT(*) ")
+        
+        rows = cur.fetchall()
+
+        for row in rows:
+            print(row)
+    except Error as e:
+        print(e)   
+
+def getSupervisor(conn):
+    print("For each employee who is a supervisor, retrieve the employee first and last name and number (count) of employees that are supervised. Order the result in descending order")
+    try:
+        cur = conn.cursor()
+        cur.execute("SELECT S.Fname, S.Lname, COUNT(*) FROM EMPLOYEE AS E, EMPLOYEE AS S  WHERE E.SupervisorSSN=S.EmployeeSSN GROUP BY S.EmployeeSSN ORDER BY COUNT(*) DESC ")
+        
+        rows = cur.fetchall()
+
+        for row in rows:
+            print(row)
+    except Error as e:
+        print(e)  
+
+def main():
+    #create connection
+    databaseName = "CSE3330_P1.db"
+    conn = connection(databaseName)
+
+    #Enter a department name, and retrieve all the names and salaries of all employees who work in that department.
+    getNameSalaries(conn, "Research")
+
+    #Enter an employee last name and first name and retrieve a list of projects names/hours per week that the employee works on
+    getProjNameHours(conn, "Wong", "Franklin")
+    
+    #Enter a department name and retrieve the total of all employee salaries who work in the department
+    getTotalSalaries(conn, "Research")
+
+    #For each department, retrieve the department name and the number (count) of employees who work in that department. Order the result by number of employees in descending order
+    getAllDeptEmp(conn)
+
+    # For each employee who is a supervisor, retrieve the employee first and last name and number (count) of employees that are supervised. Order the result in descending order.
+    getSupervisor(conn)
+
+
+if __name__ == '__main__':
+    main()
